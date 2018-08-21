514faa7ef35c039e410f83ab5ce35002a8b50b1e
luoChen <hank.luo@xianghuanji.com>
2018-08-21 16:25:31 +0800

    module-user 可以正常运行

---
+++ example/module-user/src/main/java/com/niluogege/example/module_user/DemoActivity.java
@@ -0,0 +1,17 @@
+package com.niluogege.example.module_user;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by niluogege on 2018/8/20.
+ */
+
+public class DemoActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_activity_main);
+    }
+}
